4e5c8e9adae3c7ad70c6ec7f3f36eb927777c447
Brumz <bmanrumsey24@gmail.com>
2019-01-05 16:22:54 -0700

    fixed heroku error

--- knexfile.js
+++ knexfile.js
@@ -1,11 +1,10 @@
 module.exports = {
-
   development: {
     client: "pg",
     connection: "postgresql://localhost/games"
   },
 
-  staging: {
+  production: {
     client: "pg",
     connection: process.env.DATABASE_URL
   }
